7ebf531e6a8eebc0a9a0b9c5e0d1c0801cd88213
Ygor <ygorespada14@gmail.com>
2023-08-17 10:40:38 -0300

    Calculadora Completa!

--- app/src/main/java/br/edu/up/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/up/calculadora/MainActivity.kt
@@ -2,10 +2,39 @@ package br.edu.up.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val valor1 = findViewById<EditText>(R.id.valor1)
+        val valor2 = findViewById<EditText>(R.id.valor2)
+        val resultado = findViewById<TextView>(R.id.resultado)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val executar = findViewById<Button>(R.id.botao)
+
+        executar.setOnClickListener {
+            val valor1 = valor1.text.toString().toDoubleOrNull()
+            val valor2 = valor2.text.toString().toDoubleOrNull()
+
+            if (valor1 != null && valor2 != null) {
+                val selectedOperation = when (radioGroup.checkedRadioButtonId) {
+                    R.id.somar -> valor1 + valor2
+                    R.id.subtrair -> valor1 - valor2
+                    R.id.multiplicar -> valor1 * valor2
+                    R.id.dividir -> if (valor2 != 0.0) valor1 / valor2 else Double.NaN
+                    else -> Double.NaN
+                }
+
+                resultado.text = "Resultado: $selectedOperation"
+            } else {
+                resultado.text = "Valores inválidos"
+            }
+        }
     }
 }
