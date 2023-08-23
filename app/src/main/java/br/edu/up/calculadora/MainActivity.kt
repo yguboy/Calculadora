939e00d59cabaaba86a50a3f715f9c78a862d415
Ygor <ygorespada14@gmail.com>
2023-08-23 10:30:36 -0300

    Calculadora Completa!

--- app/src/main/java/br/edu/up/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/up/calculadora/MainActivity.kt
@@ -2,8 +2,10 @@ package br.edu.up.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 
